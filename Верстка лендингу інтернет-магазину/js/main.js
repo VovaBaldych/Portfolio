6adcf85d8711c69c92224cfab1c5b2531ed59f74
VovaBaldych <48417424+VovaBaldych@users.noreply.github.com>
2020-04-09 11:32:00 -0100

    v.1.0

--- Верстка лендингу інтернет-магазину/js/main.js
+++ Верстка лендингу інтернет-магазину/js/main.js
@@ -5,6 +5,39 @@ $('.sliders').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 50000,
     infinite: true
 });
+$('.button-section').on('click', function(){
+    $('ul.top-menu').slideToggle();
+});
+if($(window).width() > 768) {
+    $('.most-popular-products').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true
+    });
+} else if($(window).width() <= 768 && $(window).width() >=576) {
+    $('.most-popular-products').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true
+    });
+} else {
+    $('.most-popular-products').slick({
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true
+    });
+}
