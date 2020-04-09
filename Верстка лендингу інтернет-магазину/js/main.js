c25178eca7379a8ff2196ec06883ae0fea2e771f
VovaBaldych <48417424+VovaBaldych@users.noreply.github.com>
2020-04-09 07:06:01 -0100

    v. 0.9

---
+++ Верстка лендингу інтернет-магазину/js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+$('.sliders').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true
+});
